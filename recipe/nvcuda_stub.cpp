e81a5c692ace122b35ec3063ea4fd0c2b5be861c
Yuan Tong <tongyuan200097@gmail.com>
2024-02-22 21:52:55 +0800

    fix constexpr capture

--- recipe/nvcuda_stub.cpp
+++ recipe/nvcuda_stub.cpp
@@ -14,4 +14,16 @@ CUresult CUDAAPI cuDevicePrimaryCtxSetFlags(CUdevice, unsigned int) {
   return CUDA_ERROR_STUB_LIBRARY;
 }
 
+CUresult CUDAAPI cuCtxPushCurrent(CUcontext) {
+  return CUDA_ERROR_STUB_LIBRARY;
+}
+
+CUresult CUDAAPI cuCtxPopCurrent(CUcontext *) {
+  return CUDA_ERROR_STUB_LIBRARY;
+}
+
+CUresult CUDAAPI cuCtxGetDevice(CUdevice *) {
+  return CUDA_ERROR_STUB_LIBRARY;
+}
+
 }
